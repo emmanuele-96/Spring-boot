d5a4782c12a19a8d1bdede8632a910b600b0560d
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-05-10 15:31:54 +0200

    esercizio del 10/05/2024
    esercizio n° 1 coretto (v2/ciao/Lombardia?nome=Giuseppe)

--- demo-webapp/src/main/java/com/example/demowebapp/Esercizio2.java
+++ demo-webapp/src/main/java/com/example/demowebapp/Esercizio2.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v2")
 public class Esercizio2 {
     @GetMapping(path = "/ciao2/{provincia}")
-    public User esercizio2(
+    public String esercizio2(
             @PathVariable String provincia,
-            @RequestParam(required = true) String nome,
-            @RequestParam(required = false, defaultValue = "Ciao Giuseppe, com'è il tempo in Lombardia?") String saluto)
-    {
-        return new User(nome, provincia, saluto);
+            @RequestParam(required = true) String nome)
 
+    {
+        return "Ciao " + nome + " com'è il tempo in " + provincia + "?";
     }
 }
--- demo-webapp/src/main/java/com/example/demowebapp/User.java
+++ demo-webapp/src/main/java/com/example/demowebapp/User.java
@@ -3,11 +3,11 @@ package com.example.demowebapp;
 public class User {
    public String nome;
    public String provincia;
-   public String saluto;
+
 
     public User(String nome, String provincia, String saluto) {
         this.nome = nome;
         this.provincia = provincia;
-        this.saluto = saluto;
+
     }
 }
