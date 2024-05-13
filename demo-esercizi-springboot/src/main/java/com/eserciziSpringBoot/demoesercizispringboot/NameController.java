5e411d70cc93922b20b32d1527627149a6b972ba
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-05-13 19:38:50 +0200

    esercizio corretto del 13/05/2024
    esercizio corretto Swagger del 13/05/2024

--- demo-esercizi-springboot/src/main/java/com/eserciziSpringBoot/demoesercizispringboot/NameController.java
+++ demo-esercizi-springboot/src/main/java/com/eserciziSpringBoot/demoesercizispringboot/NameController.java
@@ -1,18 +1,21 @@
 package com.eserciziSpringBoot.demoesercizispringboot;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v4")
 public class NameController {
+    @Operation(summary = "getName", description = "restituisce il nome (name) fornito")
         @GetMapping("/name")
         public String getName (
-                @RequestParam String name) {
+               @Parameter(description = "il nome (name) da restituire") @RequestParam String name) {
             return name;
         }
-
+        @Operation(summary = "reverce", description = "restituisce il nome (name) al contrario")
         @PostMapping("/reverce")
         public String reverce (
-                @RequestParam String name) {
+               @Parameter(description = "il nome (name) che deve ritornare ma invertito") @RequestParam String name) {
             StringBuilder reverceName = new StringBuilder(name).reverse();
             return reverceName.toString();
         }
