5caad70bbb698600fb2b5f4e20f38f8e811fbc3b
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-05-15 14:52:19 +0200

    Correzione esercizio n°2 del 15/05/2024

--- demo-webapp/src/main/java/com/example/demowebapp/Esercizio2.java
+++ demo-webapp/src/main/java/com/example/demowebapp/Esercizio2.java
@@ -8,7 +8,8 @@ public class Esercizio2 {
     @GetMapping(path = "/ciao2/{provincia}")
     public String esercizio2(
             @PathVariable String provincia,
-            @RequestParam(required = true) String nome)
+            @RequestParam(required = true) String nome,
+            @RequestParam(required = false, defaultValue = "Ciao Giuseppe, com'è il tempo in Lombardia?") String saluto)
 
     {
         return "Ciao " + nome + " com'è il tempo in " + provincia + "?";
