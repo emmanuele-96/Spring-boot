be9a3e0755e964ad5e50d1bc93525838e7ba41a7
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-05-16 21:20:01 +0200

    esercizio del 16/05/2024
    esercizio Spring boot - base dati (h2) del 16/05/2024

---
+++ demo-BaseDati/src/main/java/com/example/demoBaseDati/DemoBaseDatiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoBaseDati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBaseDatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBaseDatiApplication.class, args);
+	}
+
+}
---
+++ demo-BaseDati/src/main/java/com/example/demoBaseDati/Entity/Students.java
@@ -0,0 +1,33 @@
+package com.example.demoBaseDati.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
