26555cc47ff0da3453fd62a6de7bb77148288801
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-05-19 19:25:47 +0200

    modifica del 19/05/2024
    modifica della ProgrammingLanguageRepository, aggiunta path e  descrizione

--- crud-HAL-Explorer/src/main/java/com/example/HAL/Explorer/crud/HAL/Explorer/repositories/ProgrammingLanguageRepository.java
+++ crud-HAL-Explorer/src/main/java/com/example/HAL/Explorer/crud/HAL/Explorer/repositories/ProgrammingLanguageRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
-@Description("repo-prog-languages")
+@RepositoryRestResource(path = "repo-prog-languages", collectionResourceDescription = @Description("questa è la descrizione dell' esercizio CRUD Hal-explorer(modificato)"))
+
 
 public interface ProgrammingLanguageRepository extends JpaRepository<ProgrammingLanguage, Long> {
 }
