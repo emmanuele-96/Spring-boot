807b067a47a81733f51f668492ebd83c8d439e12
emmanuele-96 <emmanuelesartorio@gmail.com>
2024-06-10 14:53:56 +0200

    modifiche del 10/06/2024
    modifiche controllers

--- demo-interceptor2/src/main/java/com/interceptor2/demo_interceptor2/controllers/BasicController.java
+++ demo-interceptor2/src/main/java/com/interceptor2/demo_interceptor2/controllers/BasicController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/welcomes")
 public class BasicController {
-    @GetMapping("")
+    @GetMapping("/")
     public String sayWelcome() {
         return "Welcome";
     }
--- demo-interceptor2/src/main/java/com/interceptor2/demo_interceptor2/controllers/MonthController.java
+++ demo-interceptor2/src/main/java/com/interceptor2/demo_interceptor2/controllers/MonthController.java
@@ -1,13 +1,15 @@
 package com.interceptor2.demo_interceptor2.controllers;
 
+import com.interceptor2.demo_interceptor2.entities.Month;
 import jakarta.persistence.GeneratedValue;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/month")
 public class MonthController {
-    private String theMonthIs(){
-        return "the month is ";
+    private Month getMonth(Month month){
+        return month;
     }
 }
